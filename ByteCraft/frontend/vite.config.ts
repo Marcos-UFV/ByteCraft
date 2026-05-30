8ad80deca016bcedc3d1ed173498ec9d805bfe2a
Marcos Aurélio <aurelio.marcos@ufv.br>
2026-05-30 08:33:02 -0300

    build: update adapta arquivos de configuração para utilizarem docker
    - Gera o executável java no formato .war, para que ele também contenha os arquivos do frontend
    - Acrescenta dependência do TypeScript
    - Remove verificação de arquivos não utilziados que estava impedindo o build do frontend

--- ByteCraft/frontend/vite.config.ts
+++ ByteCraft/frontend/vite.config.ts
@@ -4,12 +4,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
+  build: {
+    outDir: 'dist', // Gera uma pasta 'dist' dentro de /frontend
+    emptyOutDir: true, // Limpa a pasta antes de cada build
+  },
   server: {
     port: 3030, // Mantém na 8081 (conforme configurado no Spring Security)
-    open: true,
+    host: true, // ESSENCIAL: Permite conexões externas ao container
+    open: false, // RECOMENDADO: No Docker, o 'open' tenta abrir o browser DENTRO do container (e falha)
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // Backend Spring Boot na 8080
+        // No Docker, usamos o nome do serviço definido no compose
+        target: 'http://backend:8080', // Backend Spring Boot na 8080
         changeOrigin: true,
         secure: false,
       }
